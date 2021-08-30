c89fcd2b2a8e1593cb7ce7d99293758df4b82f01
Zico Kolter <zkolter@cs.cmu.edu>
2021-08-30 15:36:08 -0400

    Update setup.py
    Bump down argparse version to avoid error in colab.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Interface library for minimalist autograding site",
     python_requires=">=3.5",
     url="http://github.com/locuslab/mugrade",
-    install_requires=["numpy >= 1.15", "pytest", "argparse"],
+    install_requires=["numpy >= 1.15", "pytest", "argparse >= 1.1"],
     zip_safe=False,
 )
 
